7c23557bb1067af98d128aa5b6100f1eafac85d5
Maximilian Knoll <m.knoll@dkfz.de>
2021-06-23 20:15:02 +0200

    ...

--- R/medianAgg.R
+++ R/medianAgg.R
@@ -5,7 +5,7 @@
 #' @export
 medianAgg <-function(obj) {
     ### cols to transform
-    ww <- which(!is.na(as.numeric(obj@data[1,])) & !colnames(obj@data) %in% c("PLATTE", "VERSUCH") & !grepl("CellCycle", colnames(obj@data)))
+    ww <- which(!is.na(as.numeric(obj@data[1,])) & !colnames(obj@data) %in% c("PLATTE", "VERSUCH") & !grepl("CellCycle", colnames(obj@data)) & !grepl("CUTOFFVAR", colnames(obj@data)))
 
     id <-paste(droplevels(factor(obj@data$VERSUCH)),droplevels(factor(obj@data$PLATTE)),droplevels(factor(obj@data$WELL)))
     pos <- list()    
@@ -22,11 +22,12 @@ medianAgg <-function(obj) {
 	    cat(paste("\r     "), round(i/length(ww)*100, 2), "%")
 	}
 	i<-i+1
-	#print(colnames(obj@data)[w])
+	print(colnames(obj@data)[w])
 	tmp <- list()
 	for (q in 1:length(pos)) {
 	    res <-integer(1)    
 	    val <- obj@data[pos[[q]],w]
+	    ###
 	    fact <- 1
 	    if (length(val) > 2 && max(val[which(!is.na(val) & !is.infinite(val))]) < 1000) {
 		fact <- 100000
@@ -60,4 +61,53 @@ medianAgg <-function(obj) {
     colnames(add) <- c("VERSUCH", "PLATTE", "WELL")
     add$TREATMENT <- obj@data$TREATMENT[match(paste(add$VERSUCH,add$PLATTE, add$WELL), id)]
     data.frame(df, add)
+}
+
+
+#' @title fraction quantification 
+#'
+#' @useDynLib cmoRe
+#'
+#' @export
+fractAgg <-function(obj) {
+    ### cols to transform
+    #ww <- which(grepl("CellCycle", colnames(obj@data)) | grepl("CUTOFFVAR", colnames(obj@data))) ##TODO
+    ww <- which(grepl("CUTOFFVAR", colnames(obj@data)) & grepl("BINARY", colnames(obj@data)))
+
+    id <-paste(droplevels(factor(obj@data$VERSUCH)),droplevels(factor(obj@data$PLATTE)),droplevels(factor(obj@data$WELL)))
+    pos <- list()    
+    for (i in unique(id)) {    
+	w <- which(id == i)    
+	pos[[length(pos)+1]] <- w 
+    }    
+
+    ####binary
+    coll <- list()
+    tr <- list()
+    i <- 1
+    nm <-c()
+    for (w in ww) {
+	if (i %% 10 == 0) {
+	    cat(paste("\r     "), round(i/length(ww)*100, 2), "%")
+	}
+	i<-i+1
+	#print(colnames(obj@data)[w])
+	if (length(unique(obj@data[,w]) == 2)){
+	    nm <-c(nm, colnames(obj@data)[w])
+	    tmp <- list()
+	    for (q in 1:length(pos)) {
+		res <-integer(1)    
+		val <- obj@data[pos[[q]],w]
+		tmp[[length(tmp)+1]] <- length(which(val==1))/length(val)
+	    }
+	    coll[[length(coll)+1]] <- unlist(tmp)
+	}
+    }
+    dfH <- do.call(cbind, coll)
+    colnames(dfH) <- paste0(nm, "_FRAC")
+
+    #### cell cylce
+    ww <- which(grepl("CellCycle", colnames(obj@data)))
+
+    data.frame(dfH)
 }
