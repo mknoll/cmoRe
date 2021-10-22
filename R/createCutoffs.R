56ac70cbe148d228e11861055476f646aa5a2895
Maximilian Knoll <m.knoll@dkfz.de>
2021-10-22 10:43:45 +0200

    allow repeated processing of same cutoff object

--- R/createCutoffs.R
+++ R/createCutoffs.R
@@ -1,7 +1,7 @@
 #' @title Calculate cutoffs for filters
 #' 
 #' @export
-calcCutoffs <- function(obj, fun=c("cc", "nc", "fb"), delim="\t", rmTreatCol=c(1)) {
+calcCutoffs <- function(obj, fun=c("cc", "nc", "fb"), delim="\t", rmTreatCol=c(1), prev=NULL) {
     # FIXME -> alles hardcoded!
     ##TODO: use only specific filters
 
@@ -13,10 +13,34 @@ calcCutoffs <- function(obj, fun=c("cc", "nc", "fb"), delim="\t", rmTreatCol=c(1
     xMinGlobMaxFB=0.2    
     xMaxGlobMaxFB=0.6    
 
+    cnt <- 0
     for (v in 1:length(obj@experiment)) {    
 	for (p in 1:length(obj@experiment[[v]])) {    
+	    cnt <- cnt+1
 	    print(paste("VERSUCH: ", v, " PLATTE: ", p))    
 	    base <- obj@experiment[[v]][p]    
+
+	    ## fix previously calulcated cutoffs object
+	    fixFB<- F
+	    fixNC<-F
+	    fixCC<-F
+	    if (!is.null(prev)) {
+		if ("fb" %in% fun) {
+		    if (length(prev$dataFB[[cnt]][[1]]$data) > 0) { next }
+		    warning("Found missing data: FB!")
+		    fixFB <- T
+		}
+		if ("nc" %in% fun) {
+		    if (length(prev$dataFB[[cnt]][[1]]$data) > 0) { next }
+		    warning("Found missing data: NC!")
+		    fixNC <- T
+		}
+		if ("cc" %in% fun) {
+		    if (length(prev$dataFB[[cnt]][[1]]$data) > 0) { next }
+		    warning("Found missing data: CC!")
+		    fixCC <- T
+		}
+	    }
 
 	    ##### TREATMENTS
 	    ### FIXME: well name        
@@ -31,13 +55,17 @@ calcCutoffs <- function(obj, fun=c("cc", "nc", "fb"), delim="\t", rmTreatCol=c(1
 		file <- paste(base, "Primarieswithoutborder.txt", sep="")    
 		nrow <- getNRows(file)        
 		meta <- readSingleCol(file, "Metadata_Well",nrow=nrow, type="character",delim=delim)             
+		treat <- treat[match(meta, treat$well),]    
 		
 		if ("cc" %in% fun) {
 		    dat <- readSingleCol(file, "Intensity_IntegratedIntensity_DNA",nrow=nrow,delim=delim)       
-		    treat <- treat[match(meta, treat$well),]    
 		    df <- data.frame(val=dat, WELL=meta, VERSUCH=v, PLATTE=p, TREATMENT=treat$TREATMENT)
 		    colnames(df)[1] <- varCC
-		    dataCC[[length(dataCC)+1]] <- cellCycleFractIntegrDNAInt(df, log = T, xMinGlobMax = log(100), var = varCC)
+		    if (!fixCC) {
+			dataCC[[length(dataCC)+1]] <- cellCycleFractIntegrDNAInt(df, log = T, xMinGlobMax = log(100), var = varCC)
+		    } else {
+			prev$dataCC[[cnt]] <- cellCycleFractIntegrDNAInt(df, log = T, xMinGlobMax = log(100), var = varCC)
+		    }
 		}
 	    }
 
@@ -48,7 +76,11 @@ calcCutoffs <- function(obj, fun=c("cc", "nc", "fb"), delim="\t", rmTreatCol=c(1
 		### FIXME: well name    
 		df <- data.frame(val=dat, WELL=meta, VERSUCH=v, PLATTE=p, TREATMENT=treat$TREATMENT)
 		colnames(df)[1] <- varFB
-		dataFB[[length(dataFB)+1]] <- cellCycleFractIntegrDNAInt(df, var=varFB, xMinGlobMax=xMinGlobMaxFB, xMaxGlobMax=xMaxGlobMaxFB, cutUpper=1)
+		if (!fixFB) {
+		    dataFB[[length(dataFB)+1]] <- cellCycleFractIntegrDNAInt(df, var=varFB, xMinGlobMax=xMinGlobMaxFB, xMaxGlobMax=xMaxGlobMaxFB, cutUpper=1)
+		} else {
+		    prev$dataFB[[cnt]] <- cellCycleFractIntegrDNAInt(df, var=varFB, xMinGlobMax=xMinGlobMaxFB, xMaxGlobMax=xMaxGlobMaxFB, cutUpper=1)
+		}
 	    }
 
 	    ######################
@@ -75,12 +107,20 @@ calcCutoffs <- function(obj, fun=c("cc", "nc", "fb"), delim="\t", rmTreatCol=c(1
 				  VERSUCH=v, PLATTE=p, TREATMENT=treat$TREATMENT)
 		colnames(dfC)[1] <- "AreaShape_Area.cell"
 		df <- merge(dfC, dfN, by=c("VERSUCH", "PLATTE", "WELL", "TREATMENT", "ON", "IN"))
-		dataNC[[length(dataNC)+1]] <- ncAreaFilter(df)
+		if (!fixNC)  {
+		    dataNC[[length(dataNC)+1]] <- ncAreaFilter(df)
+		} else {
+		    prev$dataNC[[cnt]] <- ncAreaFilter(df)
+		}
 	    }
 
 	}
     }
 
-    cutoffs <- list(dataCC=dataCC, dataNC=dataNC, dataFB=dataFB)
+    if (is.null(prev)) {
+	cutoffs <- list(dataCC=dataCC, dataNC=dataNC, dataFB=dataFB)
+    } else {
+	cutoffs <- prev
+    }
     return(cutoffs)
 }
